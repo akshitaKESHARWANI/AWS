9a59bef86ad2c721176d9279c90cd91f3b5a5919
Akshita <akshita961225@gmail.com>
2021-04-20 20:19:55 +0530

    first commit

---
+++ src/main/java/com/java/repository/BooksRepository.java
@@ -0,0 +1,7 @@
+package com.java.repository;
+import org.springframework.data.repository.CrudRepository;
+
+import com.java.model.Books;
+public interface BooksRepository extends CrudRepository<Books, Integer>
+{
+}
